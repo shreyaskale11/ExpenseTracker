443272a8ea5eeedee50f1853b502faf7122975f5
Ketul <71918814+shreyaskale11@users.noreply.github.com>
2024-01-15 23:45:24 +0530

    i

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,38 @@
 
+
+import streamlit as st
+
+import firebase_admin
+from firebase_admin import credentials,db
+from firebase_admin import firestore
+from firebase_admin import auth
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("serviceAccountKey.json")
+    app = firebase_admin.initialize_app(cred)
+firestore_client = firestore.client()
+
+
+
+
+def main():
+    # page configuration 
+    st.set_page_config(page_title="OpenAI", page_icon="🖖",layout="wide")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+main()
+
+
+
+# 
